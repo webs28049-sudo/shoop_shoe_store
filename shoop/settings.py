506c7386d25485b3746bf91f1e8f9ab4ed15192b
your lamia <webs28049@gmail.com>
2025-08-24 00:42:32 +1200

    Fix allowed hosts for render

--- shoop/settings.py
+++ shoop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['shoop-shoe-store.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
